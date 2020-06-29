b08caa6553eab15216b8ff21577631369d0e40fd
Matthew Wilke <mwilke@matthews-air.attlocal.net>
2020-06-28 21:46:35 -0500

    added api helper

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello World Test Branch</h1>
-  );
+var req = new XMLHttpRequest();
+var URLhost = 'https://swapi.co/api/planets/'
+
+class App extends React.Component {
+
+	render(){
+		return (
+			<div>
+				<h1>Star Wars API Test</h1>
+			</div>
+  		);
+	}
 }
 
 export default App;
