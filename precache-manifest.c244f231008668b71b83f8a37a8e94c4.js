15608e96b7a5c61bd83c77cbaa4bb789dcec8dc8
Matthew Wilke <mwilke@matthews-air.attlocal.net>
2020-06-30 17:47:44 -0500

    Updates

---
+++ precache-manifest.c244f231008668b71b83f8a37a8e94c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65bf59b94dd9ab92e4fc55ca7a64c1f",
+    "url": "/star-wars-fun/index.html"
+  },
+  {
+    "revision": "89a970e3232ae9386bbe",
+    "url": "/star-wars-fun/static/css/main.854224e5.chunk.css"
+  },
+  {
+    "revision": "d969371cb57ac6517d5c",
+    "url": "/star-wars-fun/static/js/2.130e478b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/star-wars-fun/static/js/2.130e478b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a970e3232ae9386bbe",
+    "url": "/star-wars-fun/static/js/main.9cfc7b80.chunk.js"
+  },
+  {
+    "revision": "f412b3578fd4491c9643",
+    "url": "/star-wars-fun/static/js/runtime-main.ee19194e.js"
+  }
+]);
